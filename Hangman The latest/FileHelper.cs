6803cf6212fa92c7c7e66b8630c346320191c84d
E5-573G <E5-573G@E5-573G>
2022-09-27 21:18:05 +0300

    Add project files.

---
+++ Hangman The latest/FileHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_The_latest
+{
+    internal class FileHelper
+    {
+        internal const string filePath = "words.txt";
+        internal static void Save()
+        {
+            var words = new List<string>() {"LION", "TIGER", "ELEPHANT", "GIRAFFE"};
+            File.WriteAllLines(filePath, words);           
+        }
+
+        internal static List<string> Load()
+        {
+            return File.ReadAllLines(filePath).ToList();
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(filePath);
+            }
+        }
+    }
+}
---
+++ Hangman The latest/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Reflection.Metadata;
+using System.Security.AccessControl;
+
+namespace Hangman_The_latest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> usedLetters = new List<string>();
+            string printabledWord = "";
+            int retryCount = 0;
+            const int maxCount = 6;
+            const string wildCard = "-";
+            string wordToGuess = GetRandomWord();
+
+            FileHelper.Save();
+
+            //repalace letters with placeholders in printable version
+            foreach (var letter in wordToGuess)
+            {
+                printabledWord += wildCard;
+            }
+
+            PrintHelper.PrintWord(printabledWord);
+
+            while (true) /*game loop*/
+            {
+                string userInput = GetLetterFromUser();
+
+                if (wordToGuess.ToUpper().Contains(userInput.ToUpper()))
+                {
+                    // convert to char array to use iondex for replacing letters
+                    var charArr = printabledWord.ToCharArray();
+                    // swap _ to actual letter
+                    for (int i = 0; i < wordToGuess.Length; i++)
+                    {
+                        if (wordToGuess[i].ToString().ToUpper() == userInput.ToUpper())
+                        {
+                            charArr[i] = userInput.ToUpper()[0];
+                        }
+                    }
+                    // re-assign new value
+                    printabledWord = string.Join("", charArr);
+                }
+                else
+                {
+                    if (!usedLetters.Contains(userInput.ToUpper()))
+                    {
+                        usedLetters.Add(userInput.ToUpper());
+                    }
+                    ++retryCount;
+                   
+                }
+
+                PrintHelper.PrintWord(printabledWord);
+                PrintHelper.PrintRetries(retryCount, usedLetters);
+
+                if (!printabledWord.Contains(wildCard))
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                }
+
+                if (retryCount >= maxCount)
+                {
+                    Console.WriteLine("Game over!");
+                    return;
+                }
+
+            }
+        }
+
+        static string GetLetterFromUser()
+        {
+            string userInput;
+            while (true)  /*get letter from user*/
+            {
+                Console.WriteLine("Please enter a letter:");
+                userInput = Console.ReadLine();
+                if (string.IsNullOrEmpty(userInput))
+                {
+                    Console.WriteLine("Please enter a letter!");
+                    continue;
+                }
+                if (userInput.Length > 1)
+                {
+                    Console.WriteLine("Please enter only one letter!");
+                    continue;
+                }
+                return userInput;
+            }
+        }
+     
+        static string GetRandomWord()
+        {
+            List<string> words = FileHelper.Load();
+            Random random = new Random();
+            int index = random.Next(words.Count);
+            string wordToGuess = words[index];
+            return wordToGuess;
+        }
+    }   
+    
+}
